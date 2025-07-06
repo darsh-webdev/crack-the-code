2b839a5aa4857c7458f8ab90f5b1a40919d2c7cb
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-07-06 23:44:33 +0530

    feat: Add solution for Progress bar component in React

--- progress-bar/src/App.tsx
+++ progress-bar/src/App.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+const ProgressBar = ({ progress }: { progress: number }) => {
+  const [animatedProgress, setAnimatedProgress] = useState(0);
+  useEffect(() => {
+    setTimeout(() => setAnimatedProgress(progress), 100);
+  }, [progress]);
+
   return (
-    <>
-      <div>
-        <h1>Progress Bar</h1>
+    <div className="progress-bar-container">
+      <div
+        className="progress-bar"
+        style={{
+          // width: `${progress}%`,
+          transform: `translateX(${animatedProgress - 100}%)`,
+          color: animatedProgress < 5 ? "white" : "black",
+        }}
+        role="progressbar"
+        aria-valuenow={progress}
+        aria-valuemax={100}
+        aria-valuemin={0}
+      >
+        {progress}%
       </div>
-    </>
+    </div>
+  );
+};
+
+function App() {
+  const bars = [0, 5, 10, 30, 40, 50, 70, 80, 90, 100];
+  return (
+    <div className="App">
+      <h1>Progress Bar</h1>
+      {bars.map((value) => (
+        <ProgressBar progress={value} />
+      ))}
+    </div>
   );
 }
 
