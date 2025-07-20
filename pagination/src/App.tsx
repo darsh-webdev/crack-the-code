159c040394a02f74d672c8ee84ad1cb5b14e7247
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-07-20 15:48:34 +0530

    update: Add pagination UI and functionality with minor UI changes

--- pagination/src/App.tsx
+++ pagination/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import ProductCard from "./components/ProductCard";
+import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
 
 interface Review {
   rating: number;
@@ -48,8 +49,11 @@ interface Product {
   thumbnail: string;
 }
 
+const PAGE_SIZE = 10;
+
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     fetchData();
@@ -63,17 +67,56 @@ function App() {
     setProducts(data.products);
   };
 
+  const totalProducts = products.length;
+  const noOfPages = Math.ceil(totalProducts / PAGE_SIZE);
+  const start = currentPage * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+
+  const goToPrevPage = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const goToNextPage = () => {
+    if (currentPage === noOfPages - 1) return;
+    setCurrentPage((prev) => prev + 1);
+  };
+
   return !products.length ? (
     <h1>No Products found</h1>
   ) : (
     <div>
       <h1>Pagination</h1>
       <div className="products-container">
-        {products.map((product) => (
+        {products.slice(start, end).map((product) => (
           <div key={product.id}>
             <ProductCard image={product.thumbnail} title={product.title} />
           </div>
         ))}
+      </div>
+      <div className="pagination-container">
+        <button
+          className="page-number"
+          onClick={goToPrevPage}
+          disabled={currentPage === 0}
+        >
+          <FaChevronCircleLeft size={20} />
+        </button>
+        {[...Array(noOfPages).keys()].map((num) => (
+          <button
+            key={num}
+            className={`page-number ${currentPage === num && "active"}`}
+            onClick={() => setCurrentPage(num)}
+          >
+            {num}
+          </button>
+        ))}
+        <button
+          className="page-number"
+          onClick={goToNextPage}
+          disabled={currentPage === noOfPages - 1}
+        >
+          <FaChevronCircleRight size={20} />
+        </button>
       </div>
     </div>
   );
