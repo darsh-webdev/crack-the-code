ba56c2d578bb90b0034b019db44b30f74afb8e28
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-09-17 22:24:13 +0530

    feat: Add solution for flatten nested array function in Javascript

---
+++ flatten-nested-array/index.js
@@ -0,0 +1,11 @@
+function flattenArray(arr) {
+  return arr.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val),
+    []
+  );
+}
+
+//For the purpose of user debugging.
+console.log(flattenArray([1, [2, [3, 4], 5], 6]));
+export default flattenArray;
