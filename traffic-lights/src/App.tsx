bf08e21c60c1c335cfc61ea5d61170608b669c63
Darshan Rajput <rajputdarshanashok@gmail.com>
2026-01-25 23:32:57 +0530

    update: Add solution for Traffic Lights component in React

--- traffic-lights/src/App.tsx
+++ traffic-lights/src/App.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [currentLight, setCurrentLight] = useState("red");
+
+  useEffect(() => {
+    const timer = setTimeout(
+      () => {
+        setCurrentLight((prevLight) => {
+          if (prevLight === "red") return "yellow";
+          if (prevLight === "yellow") return "green";
+          return "red";
+        });
+      },
+      currentLight === "red" ? 3000 : currentLight === "green" ? 2000 : 1000,
+    );
+
+    return () => clearTimeout(timer);
+  }, [currentLight]);
+
   return (
     <div className="container">
       <h2 data-testid="title">Traffic Lights</h2>
@@ -12,17 +30,17 @@ function App() {
         <div
           id="red-light"
           data-testid="red-light"
-          className={`circle red-on`}
+          className={`circle ${currentLight === "red" && "red-on"}`}
         ></div>
         <div
           id="yellow-light"
           data-testid="yellow-light"
-          className={`circle yellow-on`}
+          className={`circle ${currentLight === "yellow" && "yellow-on"}`}
         ></div>
         <div
           id="green-light"
           data-testid="green-light"
-          className={`circle green-on`}
+          className={`circle ${currentLight === "green" && "green-on"}`}
         ></div>
       </div>
     </div>
