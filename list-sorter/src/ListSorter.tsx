fcf5736876209d83d26ac530dbd5ae55dcda1a73
Darshan Rajput <rajputdarshanashok@gmail.com>
2026-01-27 21:36:00 +0530

    update: Add solution for List Sorter component in React

--- list-sorter/src/ListSorter.tsx
+++ list-sorter/src/ListSorter.tsx
@@ -1,22 +1,66 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+const SORT_OPTIONS = [
+  { label: "Default", value: "default" },
+  { label: "A - Z (Alphabetical)", value: "asc" },
+  { label: "Z - A (Reverse Alphabetical)", value: "desc" },
+  { label: "Length (Shortest First)", value: "short" },
+];
+
 export default function ListSorter({
   initialList = [],
 }: {
   initialList: string[];
 }) {
+  const [sortOption, setSortOption] = useState("default");
+  const [list, setList] = useState([...initialList]);
+
+  useEffect(() => {
+    const sortList = (option: string) => {
+      let sortedList = [...initialList];
+      switch (option) {
+        case "asc":
+          sortedList.sort((a, b) => a.localeCompare(b));
+          break;
+        case "desc":
+          sortedList.sort((a, b) => b.localeCompare(a));
+          break;
+        case "short":
+          sortedList.sort((a, b) => a.length - b.length);
+          break;
+        case "default":
+        default:
+          sortedList = [...initialList];
+          break;
+      }
+
+      setList(sortedList);
+    };
+    sortList(sortOption);
+  }, [sortOption, initialList]);
+
   return (
     <div data-testid="container">
       <div>
-        <h2> List Sorter</h2>
+        <h2 data-testid="heading">List Sorter</h2>
       </div>
       <label htmlFor="sort">Sort By:</label>
-      <select id="sort" data-testid="sort-dropdown"></select>
-      {initialList.length > 0 && (
+      <select
+        id="sort"
+        data-testid="sort-dropdown"
+        onChange={(e) => setSortOption(e.target.value)}
+      >
+        {SORT_OPTIONS.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      {list.length > 0 && (
         <div>
           <ul data-testid="list">
-            {initialList.map((item, index) => (
+            {list.map((item, index) => (
               <li key={index} data-testid="list-item">
                 {item}
               </li>
