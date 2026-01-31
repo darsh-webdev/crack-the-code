586f724c6ddd63f64b47a34d5084dcedf5be5182
Darshan Rajput <rajputdarshanashok@gmail.com>
2026-01-31 19:04:29 +0530

    update: Add solution for Loading Skeleton component in React

--- loading-skeleton/src/App.tsx
+++ loading-skeleton/src/App.tsx
@@ -3,14 +3,25 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
       <h1>Animated Loading Skeleton</h1>
       <div className="card">
         {loading ? (
-          <div className="skeleton-wrapper">{/* write code here */}</div>
+          <div className="skeleton-wrapper">
+            <div className="skeleton line name"></div>
+            <div className="skeleton line bio"></div>
+          </div>
         ) : (
           <div className="content">
             <h2>John Doe</h2>
