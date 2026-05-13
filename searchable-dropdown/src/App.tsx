e01bb0ce332e0bd7b33f93a1550f045565e27d75
Darshan Rajput <rajputdarshanashok@gmail.com>
2026-05-13 21:08:02 +0530

    feat: Add setup & UI for Searchable Dropdown component in React

---
+++ searchable-dropdown/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+
+const DROPDOWN_OPTIONS = [
+  "Apple",
+  "Banana",
+  "Cherry",
+  "Date",
+  "Elderberry",
+  "Fig",
+  "Grape",
+  "Honeydew",
+];
+
+function App() {
+  const [selected, setSelected] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  // TODO: Filter options based on inputValue and exclude selected ones
+  const availableOptions = [];
+
+  return (
+    <div>
+      <h1>Searchable Dropdown</h1>
+      <div className="dropdown-wrapper">
+        {/* TODO: Render selected tags */}
+
+        <input
+          data-testid="search-input"
+          placeholder="Search fruits..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+
+        {/* TODO: Render dropdown options */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
