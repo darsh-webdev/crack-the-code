1dc6d8813d627f689d7ab81bcc77f346828cf666
Darshan Rajput <rajputdarshanashok@gmail.com>
2026-06-19 22:28:17 +0530

    feat: Add function params/args example
    Create javascript-core-concepts/functions-params-args.js demonstrating function parameters vs arguments. Adds an introduce(name, city) function that logs a template string and an example call introduce("Darshan", "Mumbai"). Includes a comment clarifying which identifiers are parameters and which are arguments.

---
+++ javascript-core-concepts/functions-params-args.js
@@ -0,0 +1,13 @@
+function introduce(name, city) {
+  console.log(`My name is ${name} and I live in ${city}`);
+}
+
+introduce("Darshan", "Mumbai");
+
+/*
+Parameters:
+name, city
+
+Arguments:
+"Darshan", "Mumbai"
+*/
