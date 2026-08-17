615b609903a6d56e431f2ffaf30638a3fa86919a
Darshan Rajput <rajputdarshanashok@gmail.com>
2026-08-17 23:20:16 +0530

    feat: Add setup for Day View Calendar component in React

---
+++ react/day-view-calendar/src/App.tsx
@@ -0,0 +1,13 @@
+import DayView from "./components/DayView";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Calendar</h1>
+      <DayView />
+    </div>
+  );
+}
+
+export default App;
